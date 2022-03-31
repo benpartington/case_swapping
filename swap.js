560c96fda08e94a50219f535cdb5504b786f47b2
Ben Partington <bdpartington@gmail.com>
2022-03-31 11:48:10 +0100

    Wrote test to check integer input returns input, does not work...

--- swap.js
+++ swap.js
@@ -1,6 +1,10 @@
 const swap = (word) => {
   if (word === word.toUpperCase()) {
   return word.toLowerCase();
+  } else if (word === word.toLowerCase()) {
+    return word.toUpperCase();
+  } else {
+    return word
   }
 }
 
--- swap.test.js
+++ swap.test.js
@@ -1,16 +1,20 @@
 const swap = require("./swap");
 
 describe("swap", () => {
-  // it("swaps case of a letter in string", () => {
-  //   expect(swap('a')).toBe('A');
-  // });
+  it("swaps case of a letter in string", () => {
+    expect(swap('a')).toBe('A');
+  });
 
-  // it("swaps case of different letter in string", () => {
-  //   expect(swap('b')).toBe('B');
-  // });
+  it("swaps case of different letter in string", () => {
+    expect(swap('b')).toBe('B');
+  });
 
   it("swaps case of letter from up to down", () => {
     expect(swap('A')).toBe('a');
+  });
+
+  it("returns integer input unchanged", () => {
+    expect(swap(12345)).toBe(12345);
   });
 
 });
